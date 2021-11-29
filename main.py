28d2259ffa5174cb5ebaff976cce45d52287777b
Alexandre Chanson <a.chanson.tours@gmail.com>
2021-11-29 13:06:10 +0100

    implementing milp model

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ from structures import TapInstance
 
 def make_problem(prob, instance, ed, et):
     n = instance.size
-    s = prob.binary_var_list([i for i in range(n)], 0, 1, ["s_" + str(i) for i in range(n)])
-    x = prob.binary_var_matrix([i for i in range(n + 2)], [i for i in range(n + 2)])
-    u = prob.integer_var_list([i for i in range(n)], 2, n, ["u_" + str(i) for i in range(n)])
+    s = prob.binary_var_list([i for i in range(n)], 0, 1, ["s_" + str(i+1) for i in range(n)])
+    x = prob.binary_var_matrix([i for i in range(n + 2)], [i for i in range(n + 2)], name=lambda i: "x_"+str(i[0])+"_"+str(i[1]))
+    u = prob.integer_var_list([i for i in range(n)], 2, n, ["u_" + str(i+1) for i in range(n)])
 
     prob.set_objective("max", sum([s[i] * instance.interest[i] for i in range(n)]))
 
-    prob.add_constraint(sum([x[i+1, j+1] * instance.dist[i][j] for j in range(n) for i in range(n)]) <= ed, ctname="distance_epsilon")
+    prob.add_constraint(sum([x[i, j] * instance.dist[i-1][j-1] for j in range(1, n+1) for i in range(1, n+1)]) <= ed, ctname="distance_epsilon")
 
     prob.add_constraint(sum([s[i] * instance.time[i] for i in range(n)]) <= et, ctname="time_epsilon")
 
@@ -25,15 +25,15 @@ def make_problem(prob, instance, ed, et):
         prob.add_constraint(sum([x[i, j] for i in filter(lambda l: l != j, range(n + 1))]) - s[j - 1] == 0, ctname='inbound' + str(j))
 
     for i in range(1, n + 1):
-        prob.add_constraint(sum([x[i, j] for j in filter(lambda l: l != j, range(n + 1))]) - s[i - 1] == 0, ctname='outbound' + str(j))
+        prob.add_constraint(sum([x[i, j] for j in filter(lambda l: l != j, range(n + 1))]) - s[i - 1] == 0, ctname='outbound' + str(i))
 
     prob.add_constraint(sum([x[0, i] for i in range(1, n + 1)]) == 1, ctname="path_start")
     prob.add_constraint(sum([x[i, n + 1] for i in range(1, n + 1)]) == 1, ctname="path_end")
-    prob.add_constraint(sum([x[i, 0] + x[n + 1, i] for i in range(n + 2)]) + x[0, n + 1] == 0, ctname="path_structure")
+    prob.add_constraint(x[0, n + 1] == 0, ctname="path_structure_0")
 
     for i in range(1, n+1):
         for j in range(1, n+1):
-            prob.add_constraint(((n - 1) * (1 - x[i, j])) - u[i - 1] + u[j - 1] >= 1)
+            prob.add_constraint(((n - 1) * (1 - x[i, j])) - u[i - 1] + u[j - 1] >= 1, ctname="mtz_" + str(i) + "," + str(j))
 
 
 
